1ca3fa0741750de3680dcaba16fbc15d18a50eae
Tommy <91548470+HungDuong01@users.noreply.github.com>
2024-08-14 17:35:17 -0400

    added clerk auth

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
-const SignInPage = () => {
-    return (
-        <div>
-            Sign In 
-        </div>
-    );
-}
+import { SignIn } from '@clerk/nextjs'
 
-export default SignInPage;
+export default function Page() {
+  return <SignIn path = "/sign-in"/>
+};
